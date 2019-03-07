1f4c57f11a4b7aeefe5f97c53c44b320fec8850a
Origami ONOUE <onoueorigami@gmail.com>
2019-03-07 19:07:13 +0900

    はい

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@ const webpack = require('webpack');
 const path = require('path')
 
 module.exports = {
+    output: {
+        library: "lets-get-arrest",
+        libraryTarget: "umd"
+    },
     module: {
         rules: [{
             test: /\.js/,
