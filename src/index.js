f1736c829d927edbe9d094ed011e4d596d14ad6c
Origami ONOUE <onoueorigami@gmail.com>
2019-03-09 17:52:34 +0900

    オリジナルの関数を追加

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ export default function (){
     alert("!");
   }
 }
+
+export const original = () => {
+  for ( ; ; ) {
+    window.alert("　∧_∧　ババババ\n（ ・ω・)=つ≡つ\n（っ ≡つ=つ\n`/　　)\n(ノΠＵ\n何回閉じても無駄ですよ～ww\nm9（＾Д＾）プギャー！！\n　byソル (@0_Infinity_)")
+  }
+}
