04fd4c57ab9e715dcf0326be735a58513d702d42
Origami ONOUE <onoueorigami@gmail.com>
2019-06-08 01:19:41 +0900

    はい

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+export default (): void => {
+    // eslint-disable-next-line no-constant-condition
+    while (1) {
+        alert("!");
+    }
+};
+
+export const original = (): void => {
+    for (;;) {
+        window.alert(
+            "　∧_∧　ババババ\n（ ・ω・)=つ≡つ\n（っ ≡つ=つ\n`/　　)\n(ノΠＵ\n何回閉じても無駄ですよ～ww\nm9（＾Д＾）プギャー！！\n　byソル (@0_Infinity_)"
+        );
+    }
+};
